47d26dac48acabb723a54a75f4d869b8a0c4b997
Colm Vize <colm@vize.ie>
2025-05-06 00:26:06 +0100

    updating python

--- cmake/projects/python/hunter.cmake
+++ cmake/projects/python/hunter.cmake
@@ -10,8 +10,8 @@ include(hunter_report_broken_package)
 hunter_add_version(
     PACKAGE_NAME python
     VERSION "v0.1.0"
-    URL "https://github.com/mloccy/python-cmake-buildsystem/archive/refs/tags/hunter-v0.1.0.tar.gz"
-    SHA1 e6d53c45340d3bd544431780dfa3a3397c77b9a8
+    URL "https://github.com/mloccy/python-cmake-buildsystem/archive/refs/tags/v0.1.0.tar.gz"
+    SHA1 42ddd73b9222f8e6dc5824b1262f31d265f079dd
 )
 
 
