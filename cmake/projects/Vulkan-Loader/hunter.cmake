53d9ce8676bff000c65ca341fa9c8f616fcc88a4
Colm Vize <colm@vize.ie>
2025-05-12 19:00:45 +0100

    updated vulkan loader to use hunter

--- cmake/projects/Vulkan-Loader/hunter.cmake
+++ cmake/projects/Vulkan-Loader/hunter.cmake
@@ -13,13 +13,23 @@ hunter_add_version(
     PACKAGE_NAME
     Vulkan-Loader
     VERSION
+    v1.4.307-p1
+    URL
+    "https://github.com/mloccy/Vulkan-Loader/archive/refs/tags/v1.4.307-p1.tar.gz"
+    SHA1
+    04d4accfc6f698edded6f89a7baa6a12c2c0de2b
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    Vulkan-Loader
+    VERSION
     1.4.307-p0
     URL
     "https://github.com/mloccy/Vulkan-Loader/archive/refs/tags/v1.4.307-p0.tar.gz"
     SHA1
     aa658c03ec4eb407b4897df6c5b9dbb7a5a93d04
 )
-
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(Vulkan-Loader)
 hunter_download(PACKAGE_NAME Vulkan-Loader)
