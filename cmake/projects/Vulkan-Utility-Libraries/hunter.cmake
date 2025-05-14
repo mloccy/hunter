288512f6f97ff405225db9243c6836e13830cf0d
Colm Vize <colm@vize.ie>
2025-05-14 18:52:49 +0100

    adding Vulkan-Utility-Libraries

---
+++ cmake/projects/Vulkan-Utility-Libraries/hunter.cmake
@@ -0,0 +1,25 @@
+# Copyright (c) 2019, Rahul Sheth
+# All rights reserved.
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    Vulkan-Utility-Libraries
+    VERSION
+    1.4.315-p0
+    URL
+    "https://github.com/mloccy/Vulkan-Utility-Libraries/archive/refs/tags/1.4.315-p0.tar.gz"
+    SHA1
+    a392bc5e99eb96534d0247ebe12a451361005f50
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(Vulkan-Utility-Libraries)
+hunter_download(PACKAGE_NAME Vulkan-Utility-Libraries)
