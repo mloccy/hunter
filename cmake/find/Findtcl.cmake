c3e063220a442d0b57893a936ed4aac3b2c87167
Colm Vize <colm@vize.ie>
2025-05-22 20:19:17 +0100

    fixing TCL find script

--- cmake/find/Findtcl.cmake
+++ cmake/find/Findtcl.cmake
@@ -14,26 +14,30 @@ find_library(
     NO_DEFAULT_PATH
 )
 
-set(interface_libraries m z)
-if(ANDROID)
-  list(APPEND interface_libraries dl)
-elseif(APPLE)
-  list(APPEND interface_libraries pthread)
-else()
-  list(APPEND interface_libraries dl pthread)
-endif()
-
-if(NOT TARGET "tcl::tcl")
-  if(NOT TCL_LIBRARY)
+if(NOT TCL_LIBRARY)
+  if (TCL_FIND_REQUIRED)
     message(FATAL_ERROR "TCL_LIBRARY not found!")
   endif()
+else()
+  set(interface_libraries m z)
+  if(ANDROID)
+  list(APPEND interface_libraries dl)
+  elseif(APPLE)
+  list(APPEND interface_libraries pthread)
+  else()
+  list(APPEND interface_libraries dl pthread)
+  endif()
+
+  if(NOT TARGET "tcl::tcl")
   add_library("tcl::tcl" UNKNOWN IMPORTED)
   set_target_properties(
-      "tcl::tcl"
-      PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${TCL_INCLUDE_DIR}"
-        IMPORTED_LOCATION "${TCL_LIBRARY}"
-        IMPORTED_LINK_INTERFACE_LIBRARIES "${interface_libraries}"
-  )
-  set(TCL_FOUND TRUE)
+    "tcl::tcl"
+    PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${TCL_INCLUDE_DIR}"
+    IMPORTED_LOCATION "${TCL_LIBRARY}"
+    IMPORTED_LINK_INTERFACE_LIBRARIES "${interface_libraries}"
+    )
+    set(TCL_FOUND TRUE)
+    endif()
+
 endif()
