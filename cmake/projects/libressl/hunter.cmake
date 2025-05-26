b9c414e3afba28422d8cadbc92c77b0a58466b4c
Colm Vize <colm@vize.ie>
2025-05-26 16:47:53 +0100

    adding LibreSSL

---
+++ cmake/projects/libressl/hunter.cmake
@@ -0,0 +1,24 @@
+# Copyright (c) 2025, Colm Vize
+# All rights reserved.
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    libressl
+    VERSION
+    4.1.0
+    URL
+    "https://github.com/libressl/portable/releases/download/v4.1.0/libressl-4.1.0.tar.gz"
+    SHA1
+    17c9a7e38f802d47c50d747f40f8c77a2bb995ac
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(hunter_venv)
+hunter_download(PACKAGE_NAME hunter_venv)
