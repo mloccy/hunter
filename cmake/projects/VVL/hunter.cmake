b04f97829992fa3de6d1e594d26862bc4a2193a7
Colm Vize <colm@vize.ie>
2025-08-18 17:33:15 +0100

    adding vulkan validation layers

--- cmake/projects/VVL/hunter.cmake
+++ cmake/projects/VVL/hunter.cmake
@@ -9,18 +9,19 @@ include(hunter_cmake_args)
 include(hunter_download)
 include(hunter_pick_scheme)
 include(hunter_cmake_args)
+
 hunter_add_version(
     PACKAGE_NAME
-    VUL
+    VVL
     VERSION
     1.4.315-p0
     URL
-    "https://github.com/mloccy/Vulkan-Utility-Libraries/archive/refs/tags/1.4.315-p0.tar.gz"
+    "https://github.com/mloccy/Vulkan-ValidationLayers/archive/refs/tags/1.4.315.tar.gz"
     SHA1
-    a392bc5e99eb96534d0247ebe12a451361005f50
+    a802209d6ba8e6fc1933f375e5b66bfc73ea1a32
 )
 
-hunter_cmake_args(VUL CMAKE_ARGS BUILD_TESTS=OFF)
+hunter_cmake_args(VVL CMAKE_ARGS BUILD_TESTS=OFF)
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
-hunter_cacheable(VUL)
-hunter_download(PACKAGE_NAME VUL)
+hunter_cacheable(VVL)
+hunter_download(PACKAGE_NAME VVL)
